7b7325d9ea251c798a8e52b1ca320c28eb06cfe5
Pnayiturik <p.nayiturik@alustudent.com>
2024-05-16 18:02:14 +0200

    airbnb

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+import sys
+sys.path.append('..')
+
+class City(BaseModel):
+    """City class that inherits from BaseModel."""
+    state_id = ""
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+import sys 
+
+class State(BaseModel):
+    """State class that inherits from BaseModel."""
+    name = ""
